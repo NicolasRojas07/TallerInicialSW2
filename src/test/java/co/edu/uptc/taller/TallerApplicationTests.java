a14a780a6e0296295628fe4d226c495f52a92232
nicolas <nicolas.rojas07@uptc.edu.co>
2026-02-17 11:02:52 -0500

    architecture

---
+++ src/test/java/co/edu/uptc/taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
